81570df5d4a1db850e4b1e74168fd072afa57711
Anchousfish <anchousfish@gmail.com>
2022-05-26 21:35:40 +0300

    v.1

--- cache.go
+++ cache.go
@@ -2,22 +2,52 @@ package cache
 
 import "time"
 
+type Element struct {
+	v        string
+	deadline time.Time
+}
 type Cache struct {
+	e map[string]Element
 }
 
 func NewCache() Cache {
+
 	return Cache{}
 }
 
-func (receiver) Get(key string) (string, bool) {
+func (c Cache) Get(key string) (string, bool) {
+	elem := c.e[key].v
+
+	if c.e[key].deadline.After(time.Now()) {
+		return elem, true
+	} else {
+		delete(c.e, key)
+		return elem, false
+	}
 
 }
 
-func (receiver) Put(key, value string) {
+func (c *Cache) Put(key, value string) {
+
+	newb := Element{value, time.Now().AddDate(200, 0, 0)}
+	c.e[key] = newb
 }
 
-func (receiver) Keys() []string {
+func (c *Cache) Keys() []string {
+	keys := make([]string, 10)
+	for i := range c.e {
+		if c.e[i].deadline.After(time.Now()) {
+			keys = append(keys, i)
+		} else {
+			delete(c.e, i)
+		}
+
+	}
+	return keys
 }
 
-func (receiver) PutTill(key, value string, deadline time.Time) {
+func (c *Cache) PutTill(key, value string, deadline time.Time) {
+	newb := Element{value, deadline}
+	c.e[key] = newb
+
 }
